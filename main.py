dafb973123a2465c4f691dd39778f62e9f7c5995
DonAllenII <don.a.alexanderii@gmail.com>
2023-10-11 18:37:38 -0400

    add value class

---
+++ main.py
@@ -0,0 +1,9 @@
+class Value():
+    def __init__(self, name):
+        self.name = name
+        self.value = None
+
+rentalIncome = Value('rental income')
+laundryIncome = Value('laundry income')
+storageIncome = Value('storage income')
+miscIncome = Value('miscellaneous income')
