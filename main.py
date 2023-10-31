8c7ee86fd4e930bd6e397c10435fc65ea54f57f1
DonAllenII <don.a.alexanderii@gmail.com>
2023-10-31 14:40:00 -0400

    commented out code

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from IPython.display import clear_output
 
-
+#initialize base Value class
 class Value():
     def __init__(self, name):
         self.name = name
         self.value = None
 
+#instantiating instances of Value for Income Class
 rentalIncome = Value('rental income')
 laundryIncome = Value('laundry income')
 storageIncome = Value('storage income')
 miscIncome = Value('miscellaneous income')
 
-
+#initialize Income class
 class Income():
     def __init__(self, rentalIncome, laundryIncome, storageIncome, miscIncome):
         self.rentalIncome = rentalIncome
@@ -21,7 +22,7 @@ class Income():
         self.totaledIncome = 0
         self.incomes = [self.rentalIncome, self.laundryIncome, self.storageIncome, self.miscIncome]
     
-
+    #method for iterating thru assigning values to types of incomes
     def initial(self):
         for i in self.incomes:
             clear_output()
@@ -39,6 +40,7 @@ class Income():
             if isinstance(i.value, int):
                 self.totaledIncome += i.value
 
+#instantiating instances of Value for Expenses Class
 mortgage = Value('mortgage')
 taxes = Value('taxes')
 insurance = Value('insurance')
@@ -52,7 +54,7 @@ repairs = Value('repairs')
 capEx = Value('capital expenditures')
 propManagement = Value('property management')
 
-
+#initializing Expenses Class
 class Expenses():
     def __init__(self, mortgage, taxes, insurance, water, garbage, electric, HOA, lawnCare, vacancy, repairs, capEx, propManagement):
         self.mortgage = mortgage
@@ -73,7 +75,7 @@ class Expenses():
     
 
     
-    
+    #method for iterating thru assigning values to types of expenses
     def initial(self):
         for i in self.expenses:
             clear_output()
@@ -84,34 +86,38 @@ class Expenses():
                 value = int(input(f'How much in {i.name} for this property?'))
                 i.value = value
                 print(i.value)
-                
+
+    #method for totaling expenses            
     def totalExpenses(self):
         for i in self.expenses:
             if isinstance(i.value, int):
                 self.totaledExpenses += i.value
 
-
+#initializing Cashflow class
 class Cashflow():
     def __init__(self, income, expenses):
         self.income = income
         self.expenses = expenses
 
-
+    #method for determining cashflow (income - expeneses)
     def monthlyCashflow(self):
         self.monthlyCash = self.income - self.expenses
 
+    #method for determining annual cashflow
     def annualCashflow(self):
         self.annualCash = self.monthlyCash * 12
 
+#instantiang instances of Value for ROI Class
 downpayment = Value('down payment')
 closingcosts = Value('closing costs')
 rehabBudget = Value('rehab budget')
 misc = Value('Miscellenious')
 
+#initialzing the ROI Class
 class ROI():
     def __init__(self, downpayment, closingcosts, rehabBudget, misc):
         self.downpayment = downpayment
-        self.closingcosts = closingclosts
+        self.closingcosts = closingcosts
         self.rehab = rehabBudget
         self.misc = misc
         self.invested = [self.downPayment, self.closingcosts, self.rehabBudget, self.misc]
@@ -130,10 +136,12 @@ class ROI():
 
     invested = [downPayment, closingCosts, rehabBudget, miscCosts]
 
+    #method for iterating and assigning values to ROI Class
     def initial(self):
         for i in self.invested:
             clear_output()
             action = str(input(f'Would you like to input a value for this expense ({i.name})?'))
+            print(i.name)
             if action.lower() == "no":
                 continue
             if action.lower() == "yes":
@@ -141,17 +149,18 @@ class ROI():
                 i.value = value
                 print(i.value)
 
+    #method for adding up total investment in ROI Class
     def totalInvestment(self):
         for i in invested:
             if i != None:
                 self.totalinvested += self.i
 
-
+#initializing the Handler Class
 class Handler():
     def __init__(self):
         pass
 
-
+    #method for running the rental income program
     def run(self):
         while True:
             action = str(input("What would you like to do?\n Press 'n' for new\n Press 'i' for individual\n "))
@@ -176,8 +185,13 @@ class Handler():
                 
             break
 
+#instantiating an instance of the Income class
 income = Income(rentalIncome, laundryIncome, storageIncome, miscIncome)
+#instatiating an instance of the Expenses class
 expenses = Expenses(mortgage, taxes, insurance, water, garbage, electric, HOA, lawnCare, vacancy, repairs, capEx, propManagement)
+#instantiating an instance of the ROI class
 roi = ROI(downpayment, closingcosts, rehabBudget, misc)
+
+#instantiating an instance of the Handler class, use run method
 handle = Handler()
 handle.run()
